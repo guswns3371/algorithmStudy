0f89c40d5092a175e58c35325bcac26f995c5542
hjoon <guswns3371@gmail.com>
2020-11-18 03:03:33 +0900

    [ch3] 1이 될 때까지

--- ch3/3_4.py
+++ ch3/3_4.py
@@ -1,15 +1,30 @@
-while True:
-    n, k = map(int, input().split())
 
+n, k = map(int, input().split())
+
+count = 0
+while n > k:
+    if n % k == 0:
+        n = n // k
+        count += 1
+    else:
+        n -= 1
+        count += 1
+
+count += n-1
+print(count)
+
+
+'''
+모범답안 보기 전
+    n, k = map(int, input().split())
+    
     count = 0
     while n != 1:
         if n % k == 0:
             n = n // k
             count += 1
-            print("[/]n:", n, "count:", count)
         else:
             n -= 1
             count += 1
-            print("[-]n:", n, "count:", count)
-
     print(count)
+'''
