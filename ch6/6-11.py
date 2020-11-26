62799c141f0a9e67d37fbdf887b2877771fa1386
hjoon <guswns3371@gmail.com>
2020-11-26 18:54:06 +0900

    [ch6] 성적이 낮은 순서

---
+++ ch6/6-11.py
@@ -0,0 +1,10 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    name, score = input().split()
+    lst.append([name, int(score)])
+
+lst = sorted(lst, key=lambda a: a[1])
+
+for i in lst:
+    print(i[0], end=" ")
