24b491f14c2ed985fa70874a175c5878abf50683
hjoon <guswns3371@gmail.com>
2021-01-30 22:30:18 +0900

    [ch12] 자물쇠와 열쇠

--- part03/ch12/12-4.py
+++ part03/ch12/12-4.py
@@ -2,8 +2,8 @@ import copy
 
 
 def draw(data):
-    for D_LINE in data:
-        for d in D_LINE:
+    for d_line in data:
+        for d in d_line:
             if d == 1:
                 print("●", end=" ")
             else:
@@ -12,115 +12,141 @@ def draw(data):
     print()
 
 
-def key_lock(key, lock):
-    print("key", key)
-    print("lock", lock)
-    data_print = copy.deepcopy(lock)
-    for i in range(N):
-        for j in range(N):
-            data_print[i][j] += key[i][j]
-    print("result", data_print)
-
-
 def check(key, lock):
-    for i in range(N):
-        for j in range(N):
-            if key[i][j] == 0:
-                key[i][j] = 1
+    n = len(lock)
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            if key[i][j] + lock[i][j] == 1:
+                total += key[i][j] + lock[i][j]
             else:
-                key[i][j] = 0
-
-    if key == lock:
+                return False
+    if total == n * n:
         return True
     else:
         return False
 
 
-def rotate90(data):
-    data_return = [[] for i in range(N)]
+def rotate90(key):
+    m = len(key)
+    ret = [[] for _ in range(m)]
 
-    for i in reversed(range(N)):
-        for j in range(N):
-            data_return[j].append(data[i][j])
+    for i in reversed(range(m)):
+        for j in range(m):
+            ret[j].append(key[i][j])
 
-    return data_return
+    return ret
 
 
-def rotate(data, degree):
-    data_return = copy.deepcopy(data)
+def rotate(key, degree):
+    ret = copy.deepcopy(key)
 
     for _ in range(degree // 90):
-        data_return = rotate90(data_return)
+        ret = rotate90(ret)
 
-    return data_return
+    return ret
 
 
-def move(data, direction):
-    data_return = copy.deepcopy(data)
+def move(key, direction):
+    m = len(key)
+    ret = copy.deepcopy(key)
 
     if direction == _up:
-        data_return.pop(0)
-        data_return.append([0 for i in range(N)])
+        ret.pop(0)
+        ret.append([0 for _ in range(m)])
     elif direction == _down:
-        data_return.insert(0, [0 for i in range(N)])
-        data_return.pop(N)
+        ret.insert(0, [0 for _ in range(m)])
+        ret.pop(m)
     elif direction == _left:
-        for D_LINE in data_return:
-            D_LINE.pop(0)
-            D_LINE.append(0)
+        for r_line in ret:
+            r_line.pop(0)
+            r_line.append(0)
     elif direction == _right:
-        for D_LINE in data_return:
-            D_LINE.pop(len(D_LINE) - 1)
-            D_LINE.insert(0, 0)
+        for r_line in ret:
+            r_line.pop(len(r_line) - 1)
+            r_line.insert(0, 0)
 
-    return data_return
+    return ret
 
 
-def solution(key, lock):
-    answer = True
+def direction_translate(direction):
+    if direction == _up:
+        return "up"
+    elif direction == _down:
+        return "down"
+    elif direction == _left:
+        return "left"
+    elif direction == _right:
+        return "right"
 
-    key = rotate(key, 90)
-    draw(key)
-    key_lock(key, lock)
 
-    key = move(key, _right)
-    draw(key)
-    key_lock(key, lock)
+def simulate(key, degree, direction, lock):
+    global answer
+    answer = False
+    m = len(key)
+    ret_key = copy.deepcopy(key)
+    print(f"<key> : {90 * degree}도 -> {direction_translate(direction)}이동")
+    draw(ret_key)
 
-    key = move(key, _down)
-    draw(key)
-    key_lock(key, lock)
-    answer = check(key, lock)
+    # 옮겨진 key가 모두 0이 될 경우
+    tot = 0
+    for i in range(m):
+        for j in range(m):
+            tot += ret_key[i][j]
+
+    if tot == 0:
+        answer = False
+        draw(ret_key)
+        print("------------------------------------------------False : key 가 모두 0")
+
+    ret_key = rotate(ret_key, 90 * degree)
+    print(f"{90 * degree}도 회전")
+    draw(ret_key)
+
+    if check(ret_key, lock):
+        answer = True
+        print(f"------------------------------------------------True : {90 * degree}도 회전")
+        return True
+    else:
+        print(f"------------------------------------------------False : {90 * degree}도 회전")
+
+    ret_key = move(ret_key, direction)
+    print(f"{direction_translate(direction)}으로 이동")
+    draw(ret_key)
+
+    if check(ret_key, lock):
+        answer = True
+        print(f"------------------------------------------------True : {direction_translate(direction)}으로 이동")
+        return True
+    else:
+        print(f"------------------------------------------------False : {direction_translate(direction)}으로 이동")
+
+    degree += 1
+    direction += 1
+    for deg in range(degree, 4):
+        for direct in range(direction, 4):
+            simulate(key, deg, direct, lock)
+
+    print(f"------------------------------------------------{answer} : 마지막")
     return answer
 
 
-key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-M = len(key)
-N = len(lock)
+def solution(key, lock):
+    m = len(key)
+    n = len(lock)
+    # key 의 크기를 m x m => n x n으로 변환 (빈 공간은 0으로 채운다)
+    new_key = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(m):
+        for j in range(m):
+            new_key[i][j] = key[i][j]
+
+    draw(lock)
+    return simulate(new_key, 0, 0, lock)
+
+
 _up = 0
 _down = 1
 _left = 2
 _right = 3
 
-print("key")
-draw(key)
-print("lock")
-draw(lock)
-key_lock(key, lock)
-print("------------------------------------------------------------")
-
-# for i in range(1, 5):
-#     print(90 * i)
-#     key1 = rotate(key, 90 * i)
-#     draw(key1)
-#     key_lock(key1, lock)
-# print("-------------")
-# for i in range(4):
-#     key2 = move(key, i)
-#     draw(key2)
-#     key_lock(key2, lock)
-
-print("------------------------------------------------------------")
-
-print(solution(key, lock))
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
