6f72039852eeb58f3315dfc990c0c0f8baf1194f
hjoon <guswns3371@gmail.com>
2021-03-28 21:02:08 +0900

    [ch18] 행성 터널

--- Template.md
+++ Template.md
@@ -12,6 +12,10 @@
 
 ```
 
+#### 실패 원인
+
+#### 해결 방안
+
 ---
 
 ## [모범 답안](https://github.com/ndb796/python-for-coding-test/blob/master//.py)
---
+++ part03/ch18/18-4.py
@@ -0,0 +1,45 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+def calculate_cost(A, B):
+    return min(abs(A[0] - B[0]), abs(A[1] - B[1]), abs(A[2] - B[2]))
+
+
+n = int(input())
+nodes = []
+edges = []
+total_cost = 0
+
+for _ in range(n):
+    x, y, z = map(int, input().split())
+    nodes.append((x, y, z))
+
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        edges.append((calculate_cost(nodes[i], nodes[j]), i, j))
+
+parent = [0] * (n + 1)
+for i in range(1, n + 1):
+    parent[i] = i
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        total_cost += cost
+
+print(total_cost)
