dfe6f9e5b6b86d4e5b7f2ab738582467e4796eb4
guswn <guswns3371@gmail.com>
2021-07-07 14:10:45 +0900

    [Review-ch14] 국영수

---
+++ 이코테/part03 [review]/ch14/14-1.py
@@ -0,0 +1,8 @@
+n = int(input())
+data = []
+for i in range(n):
+    data.append(input().split())
+data = sorted(data, key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for d in data:
+    print(d[0])
